e7c5160c84ab2a572e94deb4928dbd6f16b97560
olisajioke <chijioke914@gmail.com>
2023-10-31 16:36:35 -0700

    updating task 1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -12,8 +12,8 @@
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
-       	new_node = binary_tree_node(parent, value);
-	
+
+	new_node = binary_tree_node(parent, value);
 	if (parent == NULL || new_node == NULL)
 		return (NULL);
 	if (parent->left != NULL)
@@ -22,5 +22,5 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		parent->left->parent = new_node;
 	}
 	parent->left = new_node;
-	return (new_node);    
+	return (new_node);
 }
