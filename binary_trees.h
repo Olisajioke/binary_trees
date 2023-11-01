ace4d5b012b7184a521b51fe90781226333fe6da
olisajioke <chijioke914@gmail.com>
2023-11-01 14:22:36 -0700

    task 14

--- binary_trees.h
+++ binary_trees.h
@@ -2,6 +2,8 @@
 #define _BINARY_TREES_H_
 #include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
+
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -65,6 +67,7 @@ size_t binary_tree_depth(const binary_tree_t *tree);
 size_t binary_tree_size(const binary_tree_t *tree);
 size_t binary_tree_leaves(const binary_tree_t *tree);
 size_t binary_tree_nodes(const binary_tree_t *tree);
+int binary_tree_balance(const binary_tree_t *tree);
 
 
 #endif /* _BINARY_TREES_H_ */
