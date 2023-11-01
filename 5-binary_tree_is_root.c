7f268a3f770ab2c140ccbd900196358b0fff3909
Grace Nweke <lagra2001@gmail.com>
2023-10-31 17:08:02 -0700

    solving for task 5

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -10,7 +10,10 @@
 
 int binary_tree_is_root(const binary_tree_t *node)
 {
-	if (node->parent != NULL || node == NULL)
+	if (node == NULL)
 		return (0);
-	return (1);
+	else if (node->parent != NULL)
+		return (0);
+	else
+		return (1);
 }
