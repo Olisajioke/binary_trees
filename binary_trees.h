f3ba6ed04bbac75b7a27490b5e50a32e9617d6ea
olisajioke <chijioke914@gmail.com>
2023-11-02 06:13:57 -0700

    task 17

--- binary_trees.h
+++ binary_trees.h
@@ -71,5 +71,6 @@ int binary_tree_balance(const binary_tree_t *tree);
 int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 int is_perfect_recursive(const binary_tree_t *tree, size_t h, size_t level);
+binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 
 #endif /* _BINARY_TREES_H_ */
